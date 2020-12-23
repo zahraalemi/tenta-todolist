dd4a502a0308c635e8b8a94962548894321757e3
ZARA <zahraalemi@gmail.com>
2020-12-23 15:35:55 +0100

    write js for fetch data and remove and done btn

--- js/app.js
+++ js/app.js
@@ -1,5 +1,64 @@
+import UI from './ui.js';
+
+
+
+
 class App{
+    constructor(){
+        
+        this.ui = new UI;
+        this.listTask = document.querySelector('.list-group');
+        this.form = document.querySelector('.form');
+        this.input = document.querySelector('.form-control').value;
+
+
+    }
+    loadpage(){
+        this.ui.renderHtml(); 
+        this.listTask.addEventListener('click', this.iconChange);
+        this.form.addEventListener('submit', this.addTask);
+
+    }
+
+    addTask(e){
+        e.preventDefault();
+        let html = '';
+
+        html += `
+                <li class="list-group-item d-flex justify-content-between">
+                    <div class="align-items-center">
+                    <i class="far fa-circle"></i>
+                        <span class="ml-2">${this.input}</span> 
+                    </div>
+                    <div>
+                        <i class="far fa-trash-alt"></i>
+                    </div>
+                </li>
+            `;
+
+            this.listTask.innerHTML = html;
+    }
+
+    iconChange(event){
+        
+        if(event.target.classList.contains('fa-trash-alt')){
+            console.log(event.target.parentElement.parentElement)
+            this.listTask.removeChild(delTask)
+        }
+
+        if(event.target.classList.contains('fa-circle')){
+            event.target.classList.toggle('fa-check-circle')
+            event.target.classList.remove('fa-circle')
+        }else{
+            event.target.classList.toggle('fa-circle')
+            event.target.classList.remove('fa-check-circle')
+        }
+    }
+
 
 }
+
+const app = new App();
+app.loadpage();
 
 export default App;
--- js/ui.js
+++ js/ui.js
@@ -1,34 +1,63 @@
-
-
-import ZZ from './data.js';
-
+import api from './api.js';
 
 class UI{
     constructor(){
-        this.list = document.querySelector('.list-group');
+        this.doneClass = document.querySelector('.far')
+        this.listTask = document.querySelector('.list-group')
     }
+    getData() {
+        const res = fetch(api.apiAdress).then((res) => res.json());
+        return res;
+      }
 
-    renderHtml(){
-        const zz = new ZZ;
+    renderHtml(title){
+        this.getData().then((data) => {
         let html='';
-        
-        html += `
-            <li class="list-group-item d-flex justify-content-between">
-                <div class="align-items-center">
+    
+        data.todolist.forEach(task => {
+            const title = task.title;
+             if(task.done) { 
+                html += `
+                <li class="list-group-item d-flex justify-content-between">
+                    <div class="align-items-center">
                     <i class="far fa-check-circle"></i>
-                    <span class="ml-2">tas</span> 
-                </div>
-                <div>
-                    <i class="far fa-trash-alt"></i>
-                </div>
-            </li>
-        `;
+                        <span class="ml-2">${task.title}</span> 
+                    </div>
+                    <div>
+                        <i class="far fa-trash-alt"></i>
+                    </div>
+                </li>
+            `;
 
-        this.list.innerHTML = html;
+             } 
+             else{
+                html += `
+                <li class="list-group-item d-flex justify-content-between">
+                    <div class="align-items-center">
+                    <i class="far fa-circle"></i>
+                        <span class="ml-2">${task.title}</span> 
+                    </div>
+                    <div>
+                        <i class="far fa-trash-alt"></i>
+                    </div>
+                </li>
+            `;
+             }
+            
+            
+        });    
+        
+        this.listTask.innerHTML = html;
+        
+        });
+        
     }
 }
 
-const ui = new UI;
-ui.renderHtml();
+
+/* const ui = new UI;
+ui.renderHtml();  */
+
+export default UI;
 
 
